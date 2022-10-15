5ac111259872b8a69aefe939ccd02843e10735c8
i-sergh <seripp28@gmail.com>
2022-10-15 15:10:28 +0300

    cnv fix; color fix

--- paint.py
+++ paint.py
@@ -2,9 +2,18 @@ import cv2
 import numpy as np
 
 
-cnv = np.zeros( (480, 640, 3), dtype=np.uint8())
+#cnv = np.zeros( (480, 640, 3), dtype=np.uint8())
 
 cap = cv2.VideoCapture(0)
+
+WINDOW_WIDTH = 960
+WINDOW_HEIGHT = 540
+cap.set(3, WINDOW_WIDTH)
+cap.set(4, WINDOW_HEIGHT)
+
+
+cnv = np.zeros( (WINDOW_HEIGHT, WINDOW_WIDTH, 3) , dtype=np.uint8())
+
 
 def findContour(clr_low, clr_high, hsv, out, cnv, clr):
     mask = cv2.inRange(hsv, clr_low, clr_high)
@@ -14,24 +23,24 @@ def findContour(clr_low, clr_high, hsv, out, cnv, clr):
     cont = sorted(cont, key=cv2.contourArea, reverse=True)
     if len(cont)> 0:
         for i , cn in enumerate(cont):
-            if cv2.contourArea(cn) > 500:
+            if cv2.contourArea(cn) > 100:
                 cv2.drawContours(out, cont, i, clr, 2)
-                cv2.drawContours(cnv, cn, -1, clr, -1)
-                #cv2.drawContours(out, cont, i, clr, -1)
+                #cv2.drawContours(cnv, cn, -1, clr, -1)
+                cv2.drawContours(cnv, cont, i, clr, -1)
 
 
 while True:
     tr, frame = cap.read()
 
     frame = cv2.flip(frame, 2)
-    frame_ = cv2.blur(frame, (10,10) )
+    frame_ = cv2.blur(frame, (20,20) )
     frame_HSV = cv2.cvtColor( frame_, cv2.COLOR_BGR2HSV )
 
     # red
-    findContour( (0,200, 70), (7,255,255),
+    findContour( (0,150, 70), (7,255,255),
                  frame_HSV, frame, cnv, (0,0,255))
     # orange
-    findContour( (7,200, 70), (15,255,255),
+    findContour( (7,150, 70), (15,255,255),
                  frame_HSV, frame, cnv, (0,100,250))
     # yellow
     findContour( (15,190, 70), (30,255,255),
@@ -40,27 +49,27 @@ while True:
     findContour( (30, 150, 40), (65,255,255),
                  frame_HSV, frame, cnv, (50,255,100))
     # green
-    findContour( (65, 140, 10), (90,255,255),
-                 frame_HSV, frame, cnv, (255,255,0))
+    findContour( (65, 170, 50), (90,255,255),
+                 frame_HSV, frame, cnv, (50,125,10))
     # blue
-    findContour( (90, 120, 50), (140,255,255),
+    findContour( (90, 200, 100), (140,255,255),
                  frame_HSV, frame, cnv, (255,0,0))
     #findContour( (15, 70, 50), (40,255,255),
     #             frame_HSV, frame,frame, (255,0,0))
     # purple
-    findContour( (150, 120, 20), (180,255,255),
+    findContour( (150, 120, 20), (170,255,255),
                  frame_HSV, frame, cnv, (200,0,100))
     
     cv2.imshow('paint', cnv)
     cv2.imshow('camera', frame)
-    cnv[ cnv > 2 ] -= 2
+    #cnv[ cnv > 2 ] -= 2
     cnv[ cnv  > 0  ] -= 1
     key = cv2.waitKey(1)
     #print(key)
     if key == 27:
         break
     if key == 32:
-        cnv = np.zeros( (480, 640, 3), dtype=np.uint8())
+        cnv *= 0
         
     
 cv2.destroyAllWindows()
